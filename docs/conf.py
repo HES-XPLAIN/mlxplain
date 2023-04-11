2193ba20fcd0aaf315aa2350e28c813313506cc5
Rémy Marquis <remy.marquis@heig-vd.ch>
2023-04-11 13:43:27 +0200

    Added sphinx as doc generator
    * generated doc with sphinx-quickstart
    * set html_theme to 'sphinx_rtd_theme'
    * add poetry deps in 'doc' group

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "MLxplain"
+copyright = '2023, "Arthur Babey <arthur.babey@heig-vd.ch>", "Jean-Marc Boutay <jean-marc.boutay@heige.ch>", "Rémy Marquis <remy.marquis@heig-vd.ch>"'
+author = '"Arthur Babey <arthur.babey@heig-vd.ch>", "Jean-Marc Boutay <jean-marc.boutay@heige.ch>", "Rémy Marquis <remy.marquis@heig-vd.ch>"'
+release = "0.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. MLxplain documentation master file, created by
+   sphinx-quickstart on Tue Apr 11 13:47:58 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MLxplain's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
