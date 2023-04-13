e99fd07a7f27569abcd7be747428279443c885db
Rémy Marquis <remy.marquis@heig-vd.ch>
2023-04-13 14:08:22 +0200

    Added explanation and visualization

--- tabular_explainer_example.py
+++ tabular_explainer_example.py
@@ -4,7 +4,9 @@ import numpy as np
 import sklearn
 import xgboost
 from omnixai.data.tabular import Tabular
+from omnixai.explainers.tabular import TabularExplainer
 from omnixai.preprocessing.tabular import TabularTransform
+from omnixai.visualization.dashboard import Dashboard
 
 from mlxplain.explainers.tabular.agnostic.shap2 import Shap2Tabular
 
@@ -64,8 +66,55 @@ explainer = Shap2Tabular(
 test_instances = transformer.invert(test)
 test_x = test_instances[1653:1655]
 
-explanations = explainer.explain(test_x)
-print(explanations)
+# explanations = explainer.explain(test_x)
+# print(explanations)
+#
+# fig = explanations.plot()
+# fig.savefig("explanations.png")
 
-fig = explanations.plot()
-fig.savefig("explanations.png")
+# Convert the transformed data back to Tabular instances
+train_data = transformer.invert(train)
+test_data = transformer.invert(test)
+
+# Initialize a TabularExplainer
+explainers = TabularExplainer(
+    explainers=["lime", "shap2", "mace", "pdp", "ale"],  # The explainers to apply
+    mode="classification",  # The task type
+    data=train_data,  # The data for initializing the explainers
+    model=model,  # The ML model to explain
+    preprocess=lambda z: transformer.transform(
+        z
+    ),  # Converts raw features into the model inputs
+    params={
+        "mace": {"ignored_features": ["Sex", "Race", "Relationship", "Capital Loss"]}
+    },  # Additional parameters
+)
+
+# Generate explanations
+test_instances = test_data[:5]
+local_explanations = explainers.explain(X=test_instances)
+global_explanations = explainers.explain_global(
+    params={
+        "pdp": {
+            "features": [
+                "Age",
+                "Education-Num",
+                "Capital Gain",
+                "Capital Loss",
+                "Hours per week",
+                "Education",
+                "Marital Status",
+                "Occupation",
+            ]
+        }
+    }
+)
+
+# Launch a dashboard for visualization
+dashboard = Dashboard(
+    instances=test_instances,  # The instances to explain
+    local_explanations=local_explanations,  # Set the generated local explanations
+    global_explanations=global_explanations,  # Set the generated global explanations
+    class_names=class_names,  # Set class names
+)
+dashboard.show()  # Launch the dashboard
