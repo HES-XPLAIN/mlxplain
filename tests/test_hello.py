502bbb9890d150ae5a5ef097fa4cbd07859dd9d6
Rémy Marquis <remy.marquis@heig-vd.ch>
2023-04-13 08:23:43 +0200

    Added more dummy code with reStructured docstring

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,19 +1,25 @@
 import unittest
 
-from mlxplain.hello import hello_world
+from mlxplain.hello import hello_name, hello_world
 
 
 class TestHello(unittest.TestCase):
-
     """
-    Test case for the hello_world function.
+    A test case for the `hello_world()` and `hello_name(name)` functions.
     """
 
     def test_hello_world(self):
         """
-        Tests the hello_world function by asserting that the output of hello_world() is equal to "Hello, World!".
+        Test if the `hello_world()` function returns the string "Hello, World!".
         """
         self.assertEqual(hello_world(), "Hello, World!")
+
+    def test_hello_name(self):
+        """
+        Test if the `hello_name(name)` function returns the string "Hello, World!"
+        when the `name` parameter is set to "World".
+        """
+        self.assertEqual(hello_name("World"), "Hello, World!")
 
 
 if __name__ == "__main__":
