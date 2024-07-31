d394dc320dcbe53fc78088c15ea87a9984461438
Rémy Marquis <remy.marquis@heig-vd.ch>
2024-07-31 15:32:41 +0200

    Initial integration code

---
+++ mlxplain/explainers/vision/__init__.py
@@ -0,0 +1,39 @@
+# from .specific.rulesextract import RulesExtractImage
+#
+# __all__ = [
+#     "RulesExtractImage"
+# ]
+#
+# from omnixai.explainers.auto import VisionExplainer
+# from omnixai.explainers.vision.agnostic.lime import LimeImage
+# from omnixai.explainers.vision.agnostic.shap import ShapImage
+# from omnixai.explainers.vision.agnostic.pdp import PartialDependenceImage
+# from omnixai.explainers.vision.agnostic.l2x import L2XImage
+# from omnixai.explainers.vision.specific.ig import IntegratedGradientImage
+# from omnixai.explainers.vision.specific.gradcam import GradCAM, GradCAMPlus, LayerCAM
+# from omnixai.explainers.vision.specific.scorecam import ScoreCAM
+# from omnixai.explainers.vision.specific.cem import ContrastiveExplainer
+# from omnixai.explainers.vision.specific.smoothgrad import SmoothGrad
+# from omnixai.explainers.vision.specific.guided_bp import GuidedBP
+# from omnixai.explainers.vision.counterfactual.ce import CounterfactualExplainer
+# from omnixai.explainers.vision.specific.feature_visualization.visualizer import \
+#     FeatureVisualizer, FeatureMapVisualizer
+#
+# __all__ = [
+#     "VisionExplainer",
+#     "LimeImage",
+#     "ShapImage",
+#     "IntegratedGradientImage",
+#     "PartialDependenceImage",
+#     "L2XImage",
+#     "GradCAM",
+#     "GradCAMPlus",
+#     "ScoreCAM",
+#     "LayerCAM",
+#     "ContrastiveExplainer",
+#     "SmoothGrad",
+#     "GuidedBP",
+#     "CounterfactualExplainer",
+#     "FeatureVisualizer",
+#     "FeatureMapVisualizer"
+# ]
