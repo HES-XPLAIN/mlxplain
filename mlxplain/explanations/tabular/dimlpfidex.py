0021a681ec6a780f5efe1c3575afea1b0e8e1935
Damian Boquete <damian.boquete@hotmail.com>
2024-08-07 13:23:11 +0200

    Added minimal plots

--- mlxplain/explanations/tabular/dimlpfidex.py
+++ mlxplain/explanations/tabular/dimlpfidex.py
@@ -5,7 +5,40 @@
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
 
-from omnixai.explanations.base import ExplanationBase
+import matplotlib as plt
+import plotly.graph_objects as go
+from omnixai.explanations.base import DashFigure, ExplanationBase
+
+# Rules JSON object hierarchy (what should be inside explanation):
+# {
+#   "positive index class": int
+#   "rules": object list [
+#                           {
+#                               "accuracy": float
+#                               "antecedents": object list [
+#                                                               "attribute": int
+#                                                               "inequality": boolean
+#                                                               "value": float
+#                                                          ]
+#                               "confidence": float
+#                               "coveredSamples": int list
+#                               "coveringSize": int
+#                               "fidelity": float
+#                               "outputClass": int
+#                           }
+#                       ]
+#
+# }
+
+
+# Plot ideas:
+# - 5 best rules (by covering size)
+# - 5 most covered samples
+# - 5 least covered samples
+# - rule's metrics means and stds
+# - covering size evolution graph
+# - stats plot
+# - antecedents plots (5 most used attributes, 5 least used attributes)
 
 
 class DimlpfidexExplanation(ExplanationBase):
@@ -15,14 +48,44 @@ class DimlpfidexExplanation(ExplanationBase):
         self.mode = mode
         self.explanations = explanations
 
-    def get_explanations(self):
+    def __getitem__(self, i: int):
+        assert i < len(self.explanations)
+        return "salut"
+
+    def get_explanations(self) -> dict:
         return self.explanations
 
-    def plot(self, **kwargs):
-        pass  # TODO
+    def plot(self, **kwargs) -> None:
+        figure, ax = plt.subplots()
+        rules = self.explanations["rules"]
+        idxs = []
+        covered_samples = []
 
-    def plotly_plot(self, **kwargs):
-        pass  # TODO
+        for i, rule in enumerate(rules):
+            idxs.append(f"Rule #{i}")
+            covered_samples.append(rule["coveringSize"])
 
-    def ipython_plot(self, **kwargs):
+        ax.bar(idxs, covered_samples)
+
+        ax.set_ylabel("Number of covered samples")
+        ax.set_xlabel("Rules id's")
+
+        return figure
+
+    def plotly_plot(self, **kwargs) -> None:
+        rules = self.explanations["rules"]
+        idxs = []
+        covered_samples = []
+
+        for i, rule in enumerate(rules):
+            idxs.append(f"Rule #{i}")
+            covered_samples.append(rule["coveringSize"])
+
+        fig = go.Figure(
+            data=[go.Bar(x=idxs, y=covered_samples)],
+            layout_title_text="Generated rules",
+        )
+        return DashFigure(fig)
+
+    def ipython_plot(self, **kwargs) -> None:
         pass  # TODO
