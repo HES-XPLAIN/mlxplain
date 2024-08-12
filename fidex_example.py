36412967a60e6d4c4aa3e4234d73935b7c5b2b21
ElSuperDado <damian.boquete@hotmail.com>
2024-08-12 13:23:13 +0200

    Added real dataset

--- fidex_example.py
+++ fidex_example.py
@@ -14,31 +14,84 @@ from omnixai.visualization.dashboard import Dashboard
 
 from mlxplain.explainers.tabular.specific.dimlpfidex import (  # DimlpBTModel,; GradBoostModel,; RandomForestModel,; SVMModel,; FidexAlgorithm,
     FidexGloRulesAlgorithm,
-    MLPModel,
+    SVMModel,
+    GradBoostModel,
 )
+
+
+def load_data(output_path, train_data, test_data):
+    dataset = pd.read_csv("/home/eldado/Downloads/ObesityDataSet.csv")
+
+    # reducing labels names size
+    dataset.rename(
+        columns={
+            "family_history_with_overweight": "FHWO",
+            "NObeyesdad": "OLD",
+        },
+        inplace=True,
+    )
+
+    # shuffle the entire dataset
+    dataset = dataset.sample(frac=1)
+    nrows = int(dataset.shape[0] * 0.1)
+    dataset = dataset.iloc[:nrows, :]
+
+
+    strToBinDict = {"yes": 1, "no": 0}
+
+    with pd.option_context('future.no_silent_downcasting', True):
+        dataset["FHWO"] = dataset["FHWO"].replace(strToBinDict).astype("int8")
+        dataset["FAVC"] = dataset["FAVC"].replace(strToBinDict).astype("int8")
+        dataset["SMOKE"] = dataset["SMOKE"].replace(strToBinDict).astype("int8")
+        dataset["SCC"] = dataset["SCC"].replace(strToBinDict).astype("int8")
+
+
+    adjToValDict = {"Always": 1.0, "Frequently": 0.66, "Sometimes": 0.33, "no": 0.0}
+
+    # Avoid deprecation warning for Pandas 3.x
+    with pd.option_context('future.no_silent_downcasting', True):
+        dataset["CAEC"] = dataset["CAEC"].replace(adjToValDict).astype('float64')
+        dataset["CALC"] = dataset["CALC"].replace(adjToValDict).astype('float64')
+
+
+    genderCols = pd.get_dummies(dataset["Gender"], prefix="Gender",prefix_sep='_', dtype='int8')
+    mtransCols = pd.get_dummies(dataset["MTRANS"], prefix="MTRANS",prefix_sep='_', dtype='int8')
+    oldCols = pd.get_dummies(dataset["OLD"], prefix="OLD",prefix_sep='_', dtype='int8')
+    dataset = pd.concat([genderCols, dataset.iloc[:,:16], mtransCols,  dataset.iloc[:,16:], oldCols], axis=1)
+    dataset.drop(["Gender", "MTRANS", "OLD"], axis=1, inplace=True)
+
+    labels = list(dataset.columns)
+
+    nclasses = sum(1 for label in labels if label.startswith("OLD_"))
+    nattributes = len(labels) - nclasses
+
+    nRecords = dataset.shape[0]
+    trainSplit = int(0.75 * nRecords)
+
+    trainds = dataset.iloc[:trainSplit, :]
+    testds = dataset.iloc[trainSplit:, :]
+
+    train = Tabular(
+        data=trainds,
+        feature_columns=labels
+    )
+    test = Tabular(
+        data=testds,
+        feature_columns=labels
+    )
+
+    return train, test, nattributes, nclasses
 
 if __name__ == "__main__":
     # do stuff with data
 
     # ensure path exists
     output_path = pl.Path("/tmp/explainer/")
+    train_data_file = "train_data.txt"
+    test_data_file = "test_data.txt"
 
-    # dummy datas for testing purposes
-    train_data = Tabular(
-        data=pd.DataFrame(
-            data=[[1, 2, 3, 1, 0], [4, 5, 6, 0, 1], [1, 2, 3, 1, 0], [4, 5, 6, 0, 1]],
-            columns=["a", "b", "c", "male", "female"],
-        ),
-        categorical_columns=["male", "female", "male", "female"],
-    )
+    train_data, test_data, nattributes, nclasses = load_data(output_path, train_data_file, test_data_file)
 
-    test_data = Tabular(
-        data=pd.DataFrame(
-            data=[[3, 1, 4, 1, 0], [7, 6, 4, 0, 1], [3, 1, 4, 1, 0], [7, 6, 4, 0, 1]],
-            columns=["a", "b", "c", "male", "female"],
-        ),
-        categorical_columns=["male", "female", "male", "female"],
-    )
 
     # model = DimlpBTModel(
     #     output_path,
@@ -53,28 +106,27 @@ if __name__ == "__main__":
     #     output_path,
     #     train_data,
     #     test_data,
-    #     3,
-    #     2,
-    #     seed=1,
-    #     n_estimators = 3,
-    #     loss="exponential",
-    #     learning_rate=32,
-    #     subsample=0.3,
-    #     criterion="squared_error",
-    #     max_depth="no_max_depth",
-    #     min_samples_split=0.5,
-    #     min_samples_leaf=0.5,
-    #     min_weight_fraction_leaf=0.1,
-    #     max_features=3,
-    #     max_leaf_nodes=3,
-    #     min_impurity_decrease=3,
-    #     init="zero",
-    #     verbose=12,
-    #     warm_start=True,
-    #     validation_fraction=0.2,
-    #     n_iter_no_change=35,
-    #     tol=0.1,
-    #     ccp_alpha=0.1
+    #     nattributes,
+    #     nclasses,
+    #     # seed=1,
+    #     # n_estimators = 3,
+    #     # learning_rate=32,
+    #     # subsample=0.3,
+    #     # criterion="squared_error",
+    #     # max_depth="no_max_depth",
+    #     # min_samples_split=0.5,
+    #     # min_samples_leaf=0.5,
+    #     # min_weight_fraction_leaf=0.1,
+    #     # max_features=3,
+    #     # max_leaf_nodes=3,
+    #     # min_impurity_decrease=3,
+    #     # init="zero",
+    #     # verbose=12,
+    #     # warm_start=True,
+    #     # validation_fraction=0.2,
+    #     # n_iter_no_change=35, # bug is here
+    #     # tol=0.1,
+    #     # ccp_alpha=0.1
     # )
 
     # model = RandomForestModel(
@@ -103,63 +155,63 @@ if __name__ == "__main__":
     #     max_samples=2,
     # )
 
-    # model = SVMModel(
-    #     output_path,
-    #     train_data,
-    #     test_data,
-    #     3,
-    #     2,
-    #     return_roc=True,
-    #     # positive_class_index=1,
-    #     nb_quant_levels=45,
-    #     K=0.1,
-    #     C=0.1,
-    #     kernel="sigmoid",
-    #     degree=5,
-    #     gamma=0.4,
-    #     coef0=1.3,
-    #     shrinking=False,
-    #     tol=1,
-    #     cache_size=150.2,
-    #     class_weight={0:1.2, 1:3.5},
-    #     verbose = True,
-    #     max_iterations= 50,
-    #     decision_function_shape="ovo",
-    #     break_ties=False
-    # )
-
-    model = MLPModel(
+    model = SVMModel(
         output_path,
         train_data,
         test_data,
-        3,
-        2,
+        nattributes,
+        nclasses,
+        return_roc=True,
+        positive_class_index=1,
         nb_quant_levels=45,
         K=0.1,
-        hidden_layer_sizes=[12, 13, 14],
-        activation="tanh",
-        solver="sgd",
-        alpha=0.1,
-        batch_size=2,
-        learning_rate="adaptive",
-        learning_rate_init=0.1,
-        power_t=0.2,
-        max_iterations=150,
-        shuffle=False,
-        seed=1,
-        tol=0.3,
-        verbose=True,
-        warm_start=True,
-        momentum=0.1,
-        nesterovs_momentum=False,
-        # early_stopping=True,
-        validation_fraction=0.2,
-        beta_1=0.2,
-        beta_2=0.2,
-        epsilon=0.1,
-        n_iter_no_change=1,
-        max_fun=2,
+        C=0.1,
+        kernel="sigmoid",
+        degree=5,
+        gamma=0.4,
+        coef0=1.3,
+        shrinking=False,
+        tol=1,
+        cache_size=150.2,
+        class_weight={0:1.2, 1:3.5},
+        verbose = True,
+        max_iterations= 50,
+        decision_function_shape="ovo",
+        break_ties=False
     )
+
+    # model = MLPModel(
+    #     output_path,
+    #     train_data,
+    #     test_data,
+    #     nattributes,
+    #     nclasses,
+    #     nb_quant_levels=45,
+    #     K=0.1,
+    #     hidden_layer_sizes=[12, 13, 14],
+    #     activation="tanh",
+    #     solver="sgd",
+    #     alpha=0.1,
+    #     batch_size=2,
+    #     learning_rate="adaptive",
+    #     learning_rate_init=0.1,
+    #     power_t=0.2,
+    #     max_iterations=150,
+    #     shuffle=False,
+    #     seed=1,
+    #     tol=0.3,
+    #     verbose=True,
+    #     warm_start=True,
+    #     momentum=0.1,
+    #     nesterovs_momentum=False,
+    #     early_stopping=True,
+    #     validation_fraction=0.2,
+    #     beta_1=0.2,
+    #     beta_2=0.2,
+    #     epsilon=0.1,
+    #     n_iter_no_change=1,
+    #     max_fun=2,
+    # )
 
     # algorithm = FidexAlgorithm(
     #     model,
@@ -186,22 +238,21 @@ if __name__ == "__main__":
         model,
         heuristic=1,
         seed=1,
-        attributes_file="attributes.txt",
-        max_iterations=8,
+        max_iterations=10,
         min_covering=3,
-        covering_strategy=False,
+        # covering_strategy=False,
         max_failed_attempts=35,
-        min_fidelity=0.4,
-        lowest_min_fidelity=0.4,
+        min_fidelity=0.9,
+        lowest_min_fidelity=0.8,
         dropout_dim=0.5,
         dropout_hyp=0.5,
-        decision_threshold=0.2,
-        positive_class_index=0,
+        # decision_threshold=0.2,
+        # positive_class_index=0,
         nb_quant_levels=45,
         # normalization_file = "normalization.txt",
-        mus=[1, 3],
-        sigmas=[2, 3],
-        normalization_indices=[0, 2],
+        # mus=[1, 3],
+        # sigmas=[2, 3],
+        # normalization_indices=[0, 2],
         nb_threads=4,
     )
 
