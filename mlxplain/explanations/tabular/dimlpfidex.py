0e46bb288b529e64d07dc227be85c543f28f701a
ElSuperDado <damian.boquete@hotmail.com>
2024-08-19 10:58:46 +0200

    Added and tested differents ways to plot

--- mlxplain/explanations/tabular/dimlpfidex.py
+++ mlxplain/explanations/tabular/dimlpfidex.py
@@ -5,7 +5,8 @@
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import plotly
 import plotly.graph_objects as go
 from matplotlib.figure import Figure
 from omnixai.explanations.base import DashFigure, ExplanationBase
@@ -32,22 +33,23 @@ from omnixai.explanations.base import DashFigure, ExplanationBase
 # }
 
 
-# Plot ideas:
-# - 10 best rules (by covering size)
-# - 10 most covered samples
-# - 10 least covered samples
-# - rule's metrics means, stds and other stats
-# - covering size evolution graph
-# - antecedents plots (5 most used attributes, 5 least used attributes)
-
-
+# TODO add comments
 class DimlpfidexExplanation(ExplanationBase):
 
-    def __init__(self, mode, nsamples: int, explanations: dict = {}) -> None:
+    def __init__(
+        self,
+        mode,
+        nsamples: int,
+        explanations: dict = {},
+        attribute_names: list[str] | None = None,
+        class_names: list[str] | None = None,
+    ) -> None:
         super().__init__()
         self.mode = mode
         self.nsamples = nsamples
         self.explanations = explanations
+        self.attribute_names = attribute_names
+        self.class_names = class_names
 
     def __getitem__(self, i: int):
         assert i < len(self.explanations)
@@ -84,13 +86,24 @@ class DimlpfidexExplanation(ExplanationBase):
         axes.set_ylabel("Most covered samples")
 
     def plot(self, **kwargs) -> Figure:
-        figure, (ax1, ax2) = plt.subplots(2, 2)
-        self._plot_best_rules(ax1),
-        self._plot_most_covered_samples(ax2),
+        rules = self.explanations["rules"]
+        idxs = []
+        covered_samples = []
+
+        for i, rule in enumerate(rules):
+            idxs.append(i)
+            covered_samples.append(rule["coveringSize"])
+
+        figure, axes = plt.subplots(1, 1)
+
+        axes.bar(idxs, covered_samples)
+
+        plt.xlabel("Generated rules ID")
+        plt.ylabel("Number of samples covered")
 
         return figure
 
-    def plotly_plot(self, **kwargs) -> None:
+    def _plotly_figure(self, **kwargs):
         rules = self.explanations["rules"]
         idxs = []
         covered_samples = []
@@ -108,25 +121,27 @@ class DimlpfidexExplanation(ExplanationBase):
                     y=covered_samples,
                     customdata=rule_str,
                     hovertemplate="%{customdata}",
+                    name="",
                 )
             ],
             layout_title_text="Generated rules",
         )
 
-        return DashFigure(fig)
+        return fig
+
+    def plotly_plot(self, **kwargs) -> None:
+        return DashFigure(self._plotly_figure())
 
     def ipython_plot(self, **kwargs) -> None:
-        pass  # TODO
+        plotly.offline.iplot(self._plotly_figure())
 
-    def json_rule_to_string(
-        self, i, json_rule, attribute_names=None, classes_names=None
-    ):
+    def json_rule_to_string(self, i, json_rule):
         rule = f"Rule #{i}: "
 
         for antecedant in json_rule["antecedents"]:
 
-            if attribute_names is not None:
-                attribute = attribute_names[antecedant["attribute"]]
+            if self.attribute_names is not None:
+                attribute = self.attribute_names[antecedant["attribute"]]
             else:
                 attribute = f"X{antecedant['attribute']}"
 
@@ -139,8 +154,8 @@ class DimlpfidexExplanation(ExplanationBase):
 
             rule += "{:.2f}".format(antecedant["value"]) + " "
 
-        if classes_names is not None:
-            output_class = classes_names[json_rule["outputClass"]]
+        if self.class_names is not None:
+            output_class = self.class_names[json_rule["outputClass"]]
         else:
             output_class = f"class {json_rule['outputClass']}"
 
