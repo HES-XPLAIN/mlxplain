b40f9ac310b12e905c1e8c1d6aef30ef77906e51
arthurbabey <a.babey5@gmail.com>
2024-08-28 10:28:16 +0200

    Revert "remove helloWorld?"
    This reverts commit ea6079270eeb9767880d598212e17dc3ea65945f.

---
+++ mlxplain/hello.py
@@ -0,0 +1,17 @@
+def hello_world() -> str:
+    """
+    Returns a string "Hello, World!".
+
+    :rtype: str
+    """
+    return "Hello, World!"
+
+
+def hello_name(name: str) -> str:
+    """
+    Returns a string "Hello, " + name.
+
+    :param name: A string representing the name of the person to greet.
+    :rtype: str
+    """
+    return "Hello, " + name + "!"
